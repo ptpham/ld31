a0e1094b5c60d2c6296ceb32686dba896cb0f4d3
Daniel Rothfus <danielrothfus@users.sourceforge.net>
2014-12-05 22:16:48 -0800

    Adding ability to drag camera around

--- game.js
+++ game.js
@@ -1,18 +1,51 @@
-var context;
+var TILE_SIZE = 64
+var STEP_DELAY = 1000
+var CANVAS_WIDTH = 800
+var CANVAS_HEIGHT = 600
+var MAP_WIDTH = 20
+var MAP_HEIGHT = 20
+
+var context = null
+var cameraPosition = {"x": 0, "y": 0}
+
+function clamp(num, min, max) {
+    return Math.max(Math.min(num, max), min)
+}
 
 window.onload = function() {
     window.onresize = gameRender
-    window.setInterval(gameStep, 1000)
+    window.setInterval(gameStep, STEP_DELAY)
 
-    var c = document.getElementById("gameCanvas")
-    context = c.getContext("2d")
+    var canvas = document.getElementById("gameCanvas")
+    context = canvas.getContext("2d")
+
+    canvas.onmousedown = function(event) {
+        var lastX = event.clientX
+        var lastY = event.clientY
+
+        canvas.onmousemove = function(event) {
+            cameraPosition.x += (lastX - event.clientX)
+            cameraPosition.y += (lastY - event.clientY)
+            lastX = event.clientX
+            lastY = event.clientY
+
+            cameraPosition.x = clamp(cameraPosition.x, 0, TILE_SIZE * MAP_WIDTH - CANVAS_WIDTH)
+            cameraPosition.y = clamp(cameraPosition.y, 0, TILE_SIZE * MAP_HEIGHT - CANVAS_HEIGHT)
+
+            gameRender()
+        }
+
+        canvas.onmouseup = function() {
+            canvas.onmousemove = null
+        }
+    }
 
     gameRender()
 }
 
 function gameRender() {
     context.fillStyle = "black"
-    context.fillRect(0, 0, 800, 600)
+    context.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
 }
 
 function gameStep() {
