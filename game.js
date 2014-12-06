eef350a53de84f0d7b1f3efbc3685c11701bb8ed
Daniel Rothfus <danielrothfus@users.sourceforge.net>
2014-12-05 23:50:13 -0800

    Adding randomly placed sheep

--- game.js
+++ game.js
@@ -4,14 +4,18 @@ var CANVAS_WIDTH = 800
 var CANVAS_HEIGHT = 600
 var MAP_WIDTH = 20
 var MAP_HEIGHT = 20
-
-var IMAGES = ["grass0.png", "grass1.png", "grass2.png", "grass3.png"]
+var IMAGES = [
+    "grass0.png", "grass1.png", "grass2.png", "grass3.png",
+    "sheep.png"
+]
 
 var context = null
 var cameraPosition = {"x": 0, "y": 0}
 var grassHeights = []
 var resources = {}
 var canvasDirty = true
+var sheeps = []
+var entities = []
 
 function clamp(num, min, max) {
     return Math.max(Math.min(num, max), min)
@@ -29,6 +33,43 @@ function loadImages() {
 function scheduleRefresh() {
     canvasDirty = true
     window.setTimeout(gameRender, 0)
+}
+
+function Sheep(position) {
+    var self = this
+
+    self.position = position
+    entities[position.x][position.y] = self
+    console.log(self.position)
+
+    self.render = function() {
+        context.drawImage(resources["sheep.png"], position.x * TILE_SIZE, position.y * TILE_SIZE)
+    }
+
+    return self
+}
+
+function generateEntities() {
+
+    // Make entities a 2D null array of MAP_WIDTH by MAP_HEIGHT
+    for (var x = 0; x < MAP_WIDTH; x++) {
+        var column = []
+        for (var y = 0; y < MAP_HEIGHT; y++) {
+            column.push(null)
+        }
+        entities.push(column)
+    }
+
+    // Generate random sheep
+    for (var n = 0; n < 20; n++) {
+        do {
+            var position = {
+                x: Math.floor(Math.random() * MAP_WIDTH),
+                y: Math.floor(Math.random() * MAP_HEIGHT)
+            }
+        } while(entities[position.x][position.y] !== null)
+        sheeps.push(new Sheep(position))
+    }
 }
 
 window.onload = function() {
@@ -69,6 +110,8 @@ window.onload = function() {
 
     loadImages()
 
+    generateEntities()
+
     scheduleRefresh()
 }
 
@@ -90,6 +133,10 @@ function gameRender() {
         for (var y = minY; y < maxY; y++) {
             var grassImage = "grass" + grassHeights[x][y] + ".png"
             context.drawImage(resources[grassImage], x * TILE_SIZE, y * TILE_SIZE)
+
+            if (entities[x][y] !== null) {
+                entities[x][y].render()
+            }
         }
     }
     context.restore()
