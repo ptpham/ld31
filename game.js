5d66e1424aeebaae8bc76f8a4b8a0fa1d4c6a74b
Peter Pham <ptr.pham@gmail.com>
2014-12-08 21:09:34 -0800

    Adding WASD support.

--- game.js
+++ game.js
@@ -72,11 +72,10 @@ function Farmer(sprites, grid) {
   this.handleKeyDown = function(event) {
     if (gamePaused) return;
     var offX = 0, offY = 0;
-    if (event.keyCode == 37) offX = -1;
-    if (event.keyCode == 38) offY = -1;
-    if (event.keyCode == 39) offX = 1;
-    if (event.keyCode == 40) offY = 1;
-    event.preventDefault();
+    if (event.keyCode == 37 || event.keyCode == 65) offX = -1;
+    if (event.keyCode == 38 || event.keyCode == 87) offY = -1;
+    if (event.keyCode == 39 || event.keyCode == 68) offX = 1;
+    if (event.keyCode == 40 || event.keyCode == 83) offY = 1;
 
     if (offX != 0 || offY != 0) {
       moveTo(offX, offY)
